3a0b2ab57002fe0eba684687ab1b066ef69c5d3e
Ricardo Vinicius Agostini dos Santos <ricardo.santos@unidavi.edu.br>
2025-10-07 16:11:14 -0300

    ajustre

--- dados.php
+++
@@ -1,3 +0,0 @@
-<?php
-  define("usuario","ricar10");
-  define("senha","#RIC_ar10");
--- mies.php
+++
@@ -1,8 +0,0 @@
-<?php 
-function mies($nmies) {
-    $nomemies = array("janeiro.", "fevereiro.", "março."
-                   ,"abril.", "maio.", "junho."
-                   ,"julho.", "agosto.", "setembro."
-                   ,"outubro.", "novembro.", "dezembro." );
-    return $nomemies[$nmies-1];               
-}
